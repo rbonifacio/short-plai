e158edf0d14a19483093c44d7f78c1590ad6c333
Rodrigo Bonifacio <rbonifacio123@gmail.com>
2017-10-12 10:49:12 -0300

    alguns comentarios adicionados

--- c3/F3LAE.hs
+++ c3/F3LAE.hs
@@ -2,12 +2,16 @@ module F3LAE where
 
 import Prelude hiding (lookup)
 
+-- | Several type synonymous to improve readability 
 type Name = String  
 type FormalArg = String 
 type Id = String 
 
+-- | A type for representing function declaration
 data FunDec = FunDec Name FormalArg Exp 
+     deriving(Show, Eq)
 
+-- | The abstract syntax of F3LAE  
 data Exp = Num Integer
          | Add Exp Exp 
          | Sub Exp Exp 
@@ -18,69 +22,87 @@ data Exp = Num Integer
          | LambdaApp Exp Exp
      deriving(Show, Eq)     
 
+-- | The environment with the list of deferred substitutions. 
 type DefrdSub = [(Id, Value)] 
 
+-- | The value data type.
+-- 
+-- In this language (F3LAE), an expression
+-- is reduced to a value: either a number or
+-- a closure. A closure is a lambda expression
+-- that *closes* together its definition with the
+-- pending substitutions at definition time. 
+--
 data Value = NumValue Integer
            | Closure FormalArg Exp DefrdSub  
+     deriving(Show, Eq)
 
+-- | The interpreter function \0/
 interp :: Exp -> DefrdSub -> [FunDec] -> Value
 
-interp (Num n) ds decs     = NumValue n
-    
+-- the simple interpreter for numbers. 
+interp (Num n) ds decs = NumValue n
+
+-- the interpreter for an add expression.                            
 interp (Add e1 e2) ds decs = NumValue (v1 + v2) 
   where
     NumValue v1 = interp e1 ds decs
     NumValue v2 = interp e2 ds decs 
 
+-- the interpreter for a sub expression. 
 interp (Sub e1 e2) ds decs = NumValue (v1 - v2) 
   where
     NumValue v1 = interp e1 ds decs
     NumValue v2 = interp e2 ds decs 
 
-interp (Let v e1 e2) ds decs = interp (LambdaApp (Lambda v e2) e1) ds decs
+-- the interpreter for a let expression.
+-- note here that, to improve reuse, we actually
+-- convert a let exprssion in the equivalent
+-- lambda application (ela), and then interpret it.  
+interp (Let v e1 e2) ds decs = interp ela ds decs
+  where ela = (LambdaApp (Lambda v e2) e1) 
 
+-- the interpreter for a reference to a variable.
+-- here, we make a lookup for the reference, in the
+-- list of deferred substitutions. 
 interp (Ref v) ds decs =
-  let res = lookup v ds fst
+  let res = lookup v fst ds
   in case res of
-    (Nothing)    -> error "variavel nao declarada"
+    (Nothing) -> error $ "variable " ++ v ++ " not found"
     (Just (_, value)) -> value 
 
+-- the interpreter for a function application.
+-- here, we first make a lookup for the function declaration,
+-- evaluates the actual argument (leading to the parameter pmt),  
+-- and then we interpret the function body in a new "local" environment
+-- (env). 
 interp (App n a) ds decs =
-  let res = lookup n decs (\(FunDec n _ _) -> n)
+  let res = lookup n (\(FunDec n _ _) -> n) decs
   in case res of
-    (Nothing) -> error "variaval nao declarada"
-    (Just (FunDec _ fa body)) -> interp body [(fa, interp a ds decs)] decs 
+    (Nothing) -> error $ "funtion " ++ n ++ " not found"
+    (Just (FunDec _ farg body)) -> interp body env decs
+     where
+       pmt = interp a ds decs 
+       env = [(farg, pmt)] 
 
-interp (Lambda a body) ds decs = Closure a body ds
+-- the interpreter for a lambda abstraction.
+-- that is the most interesting case (IMO). it
+-- just returns a closure!
+interp (Lambda farg body) ds decs = Closure farg body ds
 
-interp (LambdaApp e1 e2) ds decs = interp body ds2 decs 
+-- the interpreter for a lambda application.
+-- plese, infer what is going on here in this
+-- case
+interp (LambdaApp e1 e2) ds decs = interp body env decs -- we interpret considering the new environment
   where
-    (Closure a body ds0) = interp e1 ds decs
-    v2  = interp e2 ds decs
-    ds2 = (a,v2):ds0
-  
-lookup :: Id -> [a] -> (a -> String) -> Maybe a
-lookup _ [] _ = Nothing
-lookup v (x:xs) f 
+    Closure farg body ds0 = interp e1 ds decs -- we expect e1 to evaluate to a closure.
+    pmt = interp e2 ds decs                   -- ds0 is the deferred substitutions at the lambda declaration
+    env = (farg, pmt):ds0                     -- env is the original environment (ds0) + a new mapping
+
+-- a new lookup function.   
+lookup :: Id -> (a -> String) -> [a] -> Maybe a
+lookup _ f [] = Nothing
+lookup v f (x:xs)  
  | v == f x = Just x
- | otherwise = lookup v xs f
+ | otherwise = lookup v f xs
 
-
--- let x = 10
---  in let f = \y -> x + y
---   in let x = 5
---    in f 20
- 
-
-
--- let x = 10
---  in let x = 5
---   in (\y -> x + y) 20
-
--- Lazy = Outermost + Sharing
-
-
--- {with {f {+ 4 5}} f} => let f = 4 + 5 in f
-
-
-  
